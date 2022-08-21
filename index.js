8bb286cddb7bb3320dde094c36ac2728b5fb1df2
Rakesh Raushan <raushanrakesh069@gmail.com>
2022-08-22 00:28:06 +0530

    Added document content loaded

--- index.js
+++ index.js
@@ -1,21 +1,8 @@
+window.addEventListener("DOMContentLoaded", getData);
 document.getElementById("submit").addEventListener("click", getSend);
 document.getElementById("show").addEventListener("click", getData);
 
 var x = document.createElement("TBODY");
-
-function showData() {
-  var y = document.createElement("TR");
-  var z1 = document.createElement("TD");
-  var z2 = document.createElement("TD");
-  z1.innerHTML = "rakesh raushan";
-  z2.innerHTML = "rakesh.singh@gmail.com";
-  y.append(z1, z2);
-  x.appendChild(y);
-
-  document.getElementById("tbl").appendChild(x);
-}
-
-showData();
 
 function getSend() {
   const email = document.getElementById("email").value;
@@ -43,8 +30,8 @@ function getData() {
     .then(function (res) {
       for (let i = 0; i < res.data.length; i++) {
         var y = document.createElement("TR");
-        var z1 = document.createElement("TD");
         var z2 = document.createElement("TD");
+        var z1 = document.createElement("TD");
         z1.innerHTML = res.data[i].email;
         z2.innerHTML = res.data[i].name;
         y.append(z1, z2);
